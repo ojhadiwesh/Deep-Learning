e723a27503c6a1312d769094c9e0ab4dc936bf18
ojhadiwesh <ojhadiwesh@gmail.com>
2018-04-18 16:40:35 -0400

    Update CNN_practice.py

--- CNN_practice.py
+++ CNN_practice.py
@@ -55,10 +55,10 @@ test_set = test_datagen.flow_from_directory(
 
 classifier.fit_generator(
                         training_set,
-                        steps_per_epoch=8000,
+                        steps_per_epoch=8000/32,
                         epochs=25,
                         validation_data=test_set,
-                        validation_steps=2000)
+                        validation_steps=2000/32)
                         
 #making a new predition
 import numpy as np
